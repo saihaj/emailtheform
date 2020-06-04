8072979654b2962dda23a087ff463ed0b3f2a411
ਸਹਜਪ੍ਰੀਤ ਸਿੰਘ <saihajpreet.singh@gmail.com>
2020-06-04 12:00:28 -0500

    [CodeFactor] Apply fixes (#6)
    Co-authored-by: codefactor-io <support@codefactor.io>

--- example/src/App.js
+++ example/src/App.js
@@ -13,7 +13,7 @@ const App = () => (
         Want to get in touch? Fill out the form below to send us a
         message and we will get back to you!
       </p>
-      
+
       <ContactForm />
 
     </Layout>
